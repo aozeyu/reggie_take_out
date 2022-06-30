6c78472cbf94e631c99928512ab3c2a33875637a
aozeyu <3484766443@qq.com>
2022-06-30 09:59:55 +0800

    Initial commit

---
+++ src/main/java/com/itheima/reggie_take_out/config/WebMvcConfig.java
@@ -0,0 +1,11 @@
+package com.itheima.reggie_take_out.config;
+
+/**
+ * @program: reggie_take_out
+ * @description: 静态资源访问器
+ * @packagename: com.itheima.reggie_take_out.config
+ * @author: 姚泽宇
+ * @date: 2022-06-30 09:59
+ **/
+public class WebMvcConfig {
+}
