5c5387e542a5d2f785ee7656cfe0f1fbf3b19aff
aozeyu <3484766443@qq.com>
2022-06-30 15:15:37 +0800

    解决bug

---
+++ src/main/java/com/itheima/reggie_take_out/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.itheima.reggie_take_out.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie_take_out.common.R;
+import com.itheima.reggie_take_out.entity.Category;
+import com.itheima.reggie_take_out.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @program: reggie_take_out
+ * @description: 333
+ * @packagename: com.itheima.reggie_take_out.controller
+ * @author: 姚泽宇
+ * @date: 2022-06-30 14:49
+ **/
+@RestController
+@RequestMapping("/category")
+@Slf4j
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping
+    public R<String> save(@RequestBody Category category) {
+        log.info("category:{}", category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize) {
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
+    @DeleteMapping
+    public R<String> delete(Long ids) {
+        log.info("删除分类，id为：{}",ids);
+        categoryService.removeById(ids);
+        return R.success("分类信息删除成功");
+    }
+}
