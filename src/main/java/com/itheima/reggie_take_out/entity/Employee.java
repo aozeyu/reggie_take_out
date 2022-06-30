249da2c9aa6d0b8b6428c2a1767f74271a5bb2cd
aozeyu <3484766443@qq.com>
2022-06-30 10:33:10 +0800

    :art :代码提交

--- src/main/java/com/itheima/reggie_take_out/entity/Employee.java
+++ src/main/java/com/itheima/reggie_take_out/entity/Employee.java
@@ -1,5 +1,12 @@
 package com.itheima.reggie_take_out.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 /**
  * @program: reggie_take_out
  * @description: 雇员类
@@ -7,5 +14,33 @@ package com.itheima.reggie_take_out.entity;
  * @author: 姚泽宇
  * @date: 2022-06-30 10:10
  **/
-public class Employee {
+@Data
+public class Employee implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private String phone;
+
+    private String sex;
+
+    private String idNumber; //驼峰命名法 ---> 映射的字段名为 id_number
+
+    private Integer status;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Long createUser;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Long updateUser;
 }
---
+++ src/main/java/com/itheima/reggie_take_out/mapper/EmployeeMapper.java
@@ -0,0 +1,16 @@
+package com.itheima.reggie_take_out.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.reggie_take_out.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @program: reggie_take_out
+ * @description: 333
+ * @packagename: com.itheima.reggie_take_out.mapper
+ * @author: 姚泽宇
+ * @date: 2022-06-30 10:17
+ **/
+@Mapper
+public interface EmployeeMapper extends BaseMapper<Employee> {
+}
